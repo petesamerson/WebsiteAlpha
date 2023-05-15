6c0c290705a548c047fc9f336cc881815ba8054e
petesamerson <swpeterson623@comcast.net>
2023-05-15 14:46:25 -0400

    physics woah

--- Scene2.js
+++ Scene2.js
@@ -7,9 +7,9 @@ class Scene2 extends Phaser.Scene {
 		this.background = this.add.tileSprite(0,0,256,275,"background")
 		this.background.setOrigin(0,0);
 
-		this.ship1 = this.add.sprite(this.background.width/2 - 50, this.background.height/2, "ship")
-		this.ship2 = this.add.sprite(this.background.width/2, this.background.height/2, "ship2")
-		this.ship3 = this.add.sprite(this.background.width/2 + 50, this.background.height/2, "ship3")
+		this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship")
+		this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2")
+		this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3")
 
 		this.anims.create({
 			key:"ship1_anim",
@@ -38,22 +38,61 @@ class Scene2 extends Phaser.Scene {
 			hideOnComplete: true
 		});
 
+	    this.anims.create({
+	      key: "red",
+	      frames: this.anims.generateFrameNumbers("power-up", {
+	        start: 0,
+	        end: 1
+	      }),
+	      frameRate: 20,
+	      repeat: -1
+	    });
+	    this.anims.create({
+	      key: "gray",
+	      frames: this.anims.generateFrameNumbers("power-up", {
+	        start: 2,
+	        end: 3
+	      }),
+	      frameRate: 20,
+	      repeat: -1
+	    });
+
 		this.add.text(20, 20, "Playing Game", {font: "25px Arial", fill: "yellow"});
 
 		this.ship1.play("ship1_anim")
 		this.ship2.play("ship2_anim")
 		this.ship3.play("ship3_anim")
+		console.log(this.ship1)
 
 		this.ship1.setInteractive()
 		this.ship2.setInteractive()
 		this.ship3.setInteractive()
 
 		this.input.on('gameobjectdown', this.destroyShip, this)
+
+		this.physics.world.setBoundsCollision();
+		this.powerUps = this.physics.add.group();
+
+		var maxObjects = 4;
+		for (var i=0; i <= maxObjects; i++) {
+			var powerUp = this.physics.add.sprite(16,16,"power-up")
+			this.powerUps.add(powerUp)
+			powerUp.setRandomPosition(0, 0, game.config.width, game.config.height)
+			if (Math.random() > 0.5) {
+				powerUp.play("red")
+			} else {
+				powerUp.play("gray")
+			}
+			powerUp.setVelocity(100, 100)	
+			powerUp.setCollideWorldBounds(true)
+			powerUp.setBounce(1)
+		}
+
 	}
 
 	moveShip(ship, speed) {
 		ship.y += speed
-		if(ship.y > this.background.height) {
+		if(ship.y > config.height) {
 			this.resetShipPos(ship)
 		}
 	}
